813b2c54268b243f2ec9fe052948cb3d6051d622
StephanieToraldeSGT <toralde.sg@obmontessori.edu.ph>
2026-04-07 13:36:42 +0800

    Add files via upload

---
+++ main3.js
@@ -0,0 +1,15 @@
+function login_access() {
+    let username = 'autumnleaves99';
+    let pwd = '01022010woah';
+
+    if (username = 'autumnleaves') (pwd = '01022010woah') => {
+
+        window.alert('Welcome autumnleaves99!');
+    }
+    else {
+        window.alert('Incorrect/No username and/or password.');
+    }
+
+}
+
+
